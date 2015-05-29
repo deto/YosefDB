e1f05805d0cbbce008b7944b0e2d509d0ecc5b7a
David DeTomaso <davedeto@gmail.com>
2015-05-29 16:50:59 -0700

    Created a couple of views.

--- SampleLibrary/views.py
+++ SampleLibrary/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse;
+from django.template import RequestContext, loader;
 
+from .models import Sample;
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to YosefDB");
+    sample = Sample.objects.first();
+    context = {'sample': sample};
+    return render(request, 'SampleLibrary/index.html', context);
+
+
+def uploadSample(request):
+    try:
+        filename = request.POST["filename"];
+    except KeyError:
+        return render(request, 'SampleLibrary/uploadSample.html')
+
+    context = {'filename': filename};
+    return render(request, 'SampleLibrary/uploadSample.html', context);
