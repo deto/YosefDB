6906f51e02e31356582576aaad549930eee7272a
David DeTomaso <davedeto@gmail.com>
2015-06-15 17:42:38 -0700

    Browse page, w DataTables
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- SampleLibrary/urls.py
+++ SampleLibrary/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url;
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.uploadSample, name='uploadSample')
+    url(r'^Upload$', views.uploadSample, name='uploadSample'),
+    url(r'^Browse$', views.viewSample, name='viewSample'),
+    url(r'^samples_asJson$', views.samples_asJson, name='DT_Ajax'),
 ]
--- SampleLibrary/views.py
+++ SampleLibrary/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, render_to_response;
 from django.template.context_processors import csrf;
 from django.http import HttpResponse;
 from django.template import RequestContext, loader;
+from django.core import serializers;
+from django.views.decorators.csrf import csrf_exempt;
 
 from .fileOps import handle_uploaded_file;
 
@@ -24,3 +26,32 @@ def uploadSample(request):
     return render_to_response('SampleLibrary/uploadSample.html',c );
 
 
+def viewSample(request):
+    c = {}
+    c.update(csrf(request));
+    return render_to_response('SampleLibrary/viewSamples.html',c);
+
+@csrf_exempt
+def samples_asJson(request):
+    #import pdb; pdb.set_trace();
+    params = request.POST;
+
+    #Filter by number and offset.
+    start = int(params["start"]);
+    length = int(params["length"]);
+    samples = Sample.objects.all();
+    paged_samples = samples[start:(start+length)];
+    json_samples = '"data": ' + serializers.serialize('json',paged_samples);
+
+    other_DT_vals = dict({
+        "draw": int(params["draw"]),
+        "recordsTotal": Sample.objects.count(),
+        "recordsFiltered": len(samples),
+    });
+
+
+    json_list = ['"'+key+'": ' + str(val) for key,val in other_DT_vals.items()];
+    json_list.append(json_samples);
+    json = "{" + ','.join(json_list) + "}";
+
+    return HttpResponse(json, content_type='application/json');
